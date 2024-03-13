1fc2feab5a1acf621c2fd19e037b6fbd212b2229
Dawid Brzeski <112512292+Leiser619@users.noreply.github.com>
2024-03-13 13:56:41 +0100

    Working Statistic Chart

--- src/features/dashboard/statistics/StatisticInfo.tsx
+++ src/features/dashboard/statistics/StatisticInfo.tsx
@@ -3,9 +3,8 @@ import { BusinessContext, useBusinessContext } from "@/providers/businessContext
 import { useUserContext } from "@/providers/userContextProvider";
 import { Database } from "@/types/supabase";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
-import { count } from "console";
 import { useEffect, useState } from "react";
-import { VerticalChart } from "./VerticalChart";
+import { PieChart } from "./PieChart";
   
 export default function StatisticInfo() {
     const {businessId}= useBusinessContext();
@@ -13,10 +12,12 @@ export default function StatisticInfo() {
     //Getting current month visits
     const [monthlyClients, setMonthlyClinets] = useState<number>();
     let profit=0.0;
+    const [servicesToChart,setServicesToChart]=useState<Map<number,number>>();
     const [totalProfit,setTotalProfit]=useState<number>();
+    const [everyServiceCount,setEveryServiceCount]=useState<Map<number,number>>();
     const [newClients,setNewClients]=useState<number>();
     const [currentProfit,setCurrentProfit]=useState<number>(0);
-    const servicesMap=new Map();
+    const servicesMap=new Map<number,number>();
 
     const currentDate = new Date();
     const year = currentDate.getFullYear();
@@ -43,6 +44,7 @@ export default function StatisticInfo() {
                 if (clientsData) {
                     setMonthlyClinets(clientsData.length);
                     getCurrentMonthProfit(clientsData);
+                    getEveryServiceCount(clientsData);
                 } else {
                     console.log("Missing Data");
                     setMonthlyClinets(0);
@@ -67,6 +69,7 @@ export default function StatisticInfo() {
                 if (services) {
                     services.forEach((service)=>{
                         servicesMap.set(service.service_id,service.price)
+                        setServicesToChart(servicesMap)
                     });
                 } else {
                     console.log("Something went wrong with downloanding service data");
@@ -79,11 +82,9 @@ export default function StatisticInfo() {
 
 
     function getCurrentMonthProfit(clientsData: { service_id: number | null; end_time: string | null; }[]){
-        console.log("Mapa:"+servicesMap)
-        console.log("Data:"+clientsData)
         clientsData.forEach((visit)=>{
-            if(servicesMap.has(visit.service_id)){
-             profit+=parseFloat(servicesMap.get(visit.service_id));   
+            if(servicesMap.has(visit.service_id as any)){
+             profit+=parseFloat(servicesMap.get(visit.service_id as any) as any);   
             }else{
                 console.log("You dont have acces to this service");
             }
@@ -91,6 +92,20 @@ export default function StatisticInfo() {
         setCurrentProfit(profit)
     }
 
+
+    function getEveryServiceCount(clientsData: { service_id: number | null; end_time: string | null; }[]){
+        const serviceCount = new Map<number, number>();
+        
+        clientsData.forEach((value) => {
+            if (value.service_id !== null && serviceCount.has(value.service_id as any)) {
+                let oldValue = serviceCount.get(value.service_id as any) || 0; 
+                serviceCount.set(value.service_id, oldValue + 1);
+            } else {
+                serviceCount.set(value.service_id as any, 1);
+            }
+        });
+        setEveryServiceCount(serviceCount);
+    }
     //Getting visits in selected range time
 
 
@@ -102,7 +117,7 @@ export default function StatisticInfo() {
         <>
             <main>
                 <h1>
-                    <VerticalChart></VerticalChart>
+                    <PieChart serviceCountMap={everyServiceCount || new Map<number, number>()} serviceMap={servicesToChart || new Map<number, number>()} />
                      profit w tym miesiacu :{currentProfit}<br></br>
                      wizyty w tym miesiacu :{monthlyClients} 
                 </h1>
