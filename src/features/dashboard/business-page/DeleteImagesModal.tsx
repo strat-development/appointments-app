c6156dbca43f888fdd736bc0f0b762a4357b454e
Andrew <work.andrew.naida@gmail.com>
2024-03-28 21:10:47 +0100

    Delete images functionality

--- src/features/dashboard/business-page/DeleteImagesModal.tsx
+++ src/features/dashboard/business-page/DeleteImagesModal.tsx
@@ -5,7 +5,7 @@ import { Database } from "@/types/supabase";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import Image from "next/image";
 import { useEffect, useState } from "react";
-import { useQuery } from "react-query";
+import { useMutation, useQuery, useQueryClient } from "react-query";
 
 type Images = Database['public']['Tables']['business-images']['Row'];
 
@@ -13,6 +13,9 @@ export const DeleteImagesModal = () => {
     const supabase = createClientComponentClient<Database>();
     const [imageUrls, setImageUrls] = useState<{ publicUrl: string }[]>([]);
     const { businessId } = useBusinessContext();
+    const queryClient = useQueryClient();
+    const [selectedImages, setSelectedImages] = useState<string[]>([]);
+    const fileNames = selectedImages.map((url) => url.split('/').pop());
     const [isModalOpen, setIsModalOpen] = useState(false);
     const handleClose = () => {
         setIsModalOpen(false);
@@ -32,14 +35,50 @@ export const DeleteImagesModal = () => {
         }
     );
 
+    const deleteSelectedImagesMutation = useMutation(
+        async () => {
+            const { data, error } = await supabase
+                .from('business-images')
+                .delete()
+                .in('image_url', fileNames);
+            if (error) {
+                throw error;
+            }
+            return data;
+        },
+        {
+            onSuccess: () => {
+                queryClient.invalidateQueries('business-images');
+            }
+        }
+    );
+
+    const deleteImaegsFromStorageMutation = useMutation(
+        async () => {
+            const { data, error } = await supabaseAdmin.storage
+                .from('business-page-photos')
+                .remove(fileNames as string[]);
+            if (error) {
+                throw error;
+            }
+            return data;
+        },
+        {
+            onSuccess: () => {
+                queryClient.invalidateQueries('business-images');
+            }
+        }
+    );
+
     useEffect(() => {
         if (images) {
             Promise.all(images.map(async (image) => {
                 const { data: publicURL } = await supabaseAdmin.storage
                     .from('business-page-photos')
                     .getPublicUrl(image.image_url)
-
+                console.log(publicURL);
                 return { publicUrl: publicURL.publicUrl };
+
 
             }))
                 .then((publicUrls) => setImageUrls(publicUrls))
@@ -49,13 +88,32 @@ export const DeleteImagesModal = () => {
 
     const bodyContent = (
         <>
-            <div className="grid grid-cols-4 gap-4">
-                {imageUrls.map((image, index) => (
-                    <div key={index} className="flex gap-4 items-center">
-                        <input type="checkbox" />
-                        <Image src={image.publicUrl} width={100} height={100} alt="Business image" className="w-20 h-20" />
-                    </div>
-                ))}
+            <div className="flex flex-col gap-4 items-start ">
+                <div className="grid grid-cols-4 gap-4">
+                    {imageUrls.map((image, index) => (
+                        <div key={index} className="flex gap-4 items-center">
+                            <input
+                                type="checkbox"
+                                value={image.publicUrl}
+                                onChange={(e) => {
+                                    if (e.target.checked) {
+                                        setSelectedImages(prevSelectedImages => [...prevSelectedImages, image.publicUrl]);
+                                    } else {
+                                        setSelectedImages(prevSelectedImages => prevSelectedImages.filter((url) => url !== image.publicUrl));
+                                    }
+                                }}
+                            />
+                            <Image src={image.publicUrl} width={100} height={100} alt="Business image" className="w-20 h-20" />
+                        </div>
+                    ))}
+                </div>
+                <button onClick={() => {
+                    deleteSelectedImagesMutation.mutateAsync();
+                    deleteImaegsFromStorageMutation.mutateAsync();
+                    setIsModalOpen(false);
+                }}>
+                    Delete selected images
+                </button>
             </div>
         </>
     )
